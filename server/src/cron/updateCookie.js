4608442f142b12fe486410de0e48d3d5bc1c73ce
Ekam Ahuja <ekamahujau@gmail.com>
2022-03-11 02:37:27 +1000

    Minior changes and improvements

--- server/src/cron/updateCookie.js
+++ server/src/cron/updateCookie.js
@@ -6,8 +6,9 @@ const CronJob = cron.CronJob
 
 export const updateAuthCookie = new CronJob('0 0 * * *', async function() {
     try {
-        consola.info("Updating Auth Cookies")
+        consola.info("Updating auth cookies")
         const newAuthCookie = await cookie()
+        if (!newAuthCookie) throw new Error("Cannot generate new auth cookie")
         const updatedCookie = await Config.updateOne({}, {authCookie: newAuthCookie})
         if (updatedCookie.modifiedCount == 1) {
             consola.success("Auth Cookies updated successfully")
