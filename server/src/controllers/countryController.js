55b6f27e47f323038ccdeda715e68671d18bda2a
Ekam Ahuja <ekamahujau@gmail.com>
2022-03-21 04:13:46 +1000

    Compeleted stock page

--- server/src/controllers/countryController.js
+++ server/src/controllers/countryController.js
@@ -31,26 +31,24 @@ export async function deleteCountry(req, res, next) {
     } catch (err) {
         next(err)
     }
-    
-
 }
 
 
 
 export async function addStock(req, res, next) {
-    let { stock, countryCode } = req.body
-
-    if (!stock || !stock.length) return res.status(403).json({success: false, error: 'Must provide an array of stock'})
-    if (!countryCode || countryCode.length !== 2) return res.status(403).json({success: false, error: 'Must provide a valid country code'})
-
-    let data = stock.filter(data => data.inviteLink !== undefined && data.inviteAddress !== undefined)
-
     try {
+        let { stock, countryCode } = req.body
+
+        if (!stock || !stock.length) throw new Error('Must provide an array of stock')
+        if (!countryCode || countryCode.length !== 2) throw new Error('Must provide a valid country code')
+
+        let data = stock.filter(data => data.inviteLink !== undefined && data.inviteAddress !== undefined)
+
         const countryExists = await Country.exists({countryCode})
-        if (!countryExists) return res.status(404).json({success: false, error: 'Invalid countryCode'})
+        if (!countryExists) throw new Error('Invalid countryCode')
 
         const updatedCountryStock = await Country.updateOne({countryCode}, {$push: { stock: data }})
-        if (!updatedCountryStock) return res.stauts(401).json({success: false, error: "Stock could not be saved"})
+        if (!updatedCountryStock) throw new Error("Stock could not be saved")
 
         res.status(201).json({success: true, message: `Successfully added ${data.length} stock`, addedStock: data})
     } catch (err) {
